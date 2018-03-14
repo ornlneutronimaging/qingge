898895fd6be7e5ca8b03d596f5030b1ec481702e
JeanBilheux <bilheuxjm@ornl.gov>
2018-03-14 14:49:49 -0400

    Added cleaned up of the vdrive data. We only keep the IV and eIV columns

--- project/preparation/vdrive_handler.py
+++ project/preparation/vdrive_handler.py
@@ -1,13 +1,18 @@
 import os
 import pandas as pd
+import re
 
 
 class VDriveHandler(object):
 
     vdrive_filename = ''
+    pd_vdrive_raw_data = None  # pandas object of raw data loaded
+    pd_vdrive_data = None # pandas object of only columns of interest
 
     def __init__(self):
-        pass
+        self.vdrive_filename = ''
+        self.pd_vdrive_raw_data = None
+        self.pd_vdrive_data = None
 
     def load_vdrive(self, vdrive_filename=''):
         """load the VDrive file"""
@@ -23,4 +28,20 @@ class VDriveHandler(object):
         vdrive_raw_data = pd.read_csv(vdrive_filename,
                                sep='\t',
                                index_col=0)
-        self.vdrive_raw_data = vdrive_raw_data
+        self.pd_vdrive_raw_data = vdrive_raw_data
+
+    def keep_columns_of_interest(self):
+        """We want to only keep the I/V and eI/V columns"""
+
+        re_string = r'^I/V_\w*$'
+        name_of_columns_to_keep = []
+        full_list_name_of_columns = self.pd_vdrive_raw_data.columns.values
+        pd_vdrive_raw_data = self.pd_vdrive_raw_data.copy()
+        for _index, _label in enumerate(full_list_name_of_columns):
+            m = re.match(re_string, _label)
+            if m:
+                name_of_columns_to_keep.append(pd_vdrive_raw_data.columns.values[_index])
+                name_of_columns_to_keep.append(pd_vdrive_raw_data.columns.values[_index + 1])
+
+        self.pd_vdrive_data = pd_vdrive_raw_data.filter(name_of_columns_to_keep)
+
--- tests/project/preparation/vdrive_handler_test.py
+++ tests/project/preparation/vdrive_handler_test.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import numpy as np
+import re
 
 from project.preparation.vdrive_handler import VDriveHandler
 
@@ -29,15 +30,37 @@ class TestVDriveHandler(unittest.TestCase):
         vdrive_file = self.vdrive_filename
         o_vdrive = VDriveHandler()
         o_vdrive.load_vdrive(vdrive_filename=vdrive_file)
-        return_vdrive_raw_data = o_vdrive.vdrive_raw_data
+        returned_vdrive_raw_data = o_vdrive.pd_vdrive_raw_data
 
         # checking index (list of runs)
         expected_runs_0_10 = list(np.arange(78901.0, 78910.0))
-        returned_runs_0_10 = list(return_vdrive_raw_data.index[0:9])
+        returned_runs_0_10 = list(returned_vdrive_raw_data.index[0:9])
         self.assertEqual(expected_runs_0_10, returned_runs_0_10)
 
-        # checking column 2
-        # expected_data_col2 = list_
-        #returned_data_col2 = list(return_vdrive_raw_data.column[2])
+    def test_vdrive_data_correctly_cleaned(self):
+        """assert vdrive data keeps only the columns of interest (I/V and eI/V)"""
+        vdrive_file = self.vdrive_filename
+        o_vdrive = VDriveHandler()
+        o_vdrive.load_vdrive(vdrive_filename=vdrive_file)
+        o_vdrive.keep_columns_of_interest()
+        pd_vdrive_data = o_vdrive.pd_vdrive_data
+        list_name_of_columns = pd_vdrive_data.columns.values
+
+        # make sure there is a many IV as eIV columns
+        number_of_IV_columns = 0
+        number_of_eIV_columns = 0
+
+        IV_re_string = r'^I/V_\w*$'
+        eIV_re_string = r'^eI/V_\w*$'
+        for _index, _label in enumerate(list_name_of_columns):
+            m_IV = re.match(IV_re_string, _label)
+            if m_IV:
+                number_of_IV_columns += 1
+
+            m_eIV = re.match(eIV_re_string, _label)
+            if m_eIV:
+                number_of_eIV_columns += 1
+
+        self.assertEqual(number_of_eIV_columns, number_of_IV_columns)
 
 
