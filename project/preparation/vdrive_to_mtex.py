55935df33cddc4bfdbdcb1fc56bf8ce28c2d77d1
JeanBilheux <bilheuxjm@ornl.gov>
2018-04-16 16:58:55 -0400

    Work in progress

--- project/preparation/vdrive_to_mtex.py
+++ project/preparation/vdrive_to_mtex.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 import pandas as pd
 
@@ -5,7 +6,17 @@ import pandas as pd
 class VdriveToMtex(object):
 
     vdrive_handler_file = ''  # name of input file (produced by vdrive_handler.py
-    raw_input_data = []
+    raw_data = []
+    raw_data_sorted = []
+
+    a111 = np.zeros((12, 19))
+    a200 = np.zeros((12, 19))
+    a220 = np.zeros((12, 19))
+    a311 = np.zeros((12, 19))
+    a222 = np.zeros((12, 19))
+
+    psi = np.arange(0, 91, 5)
+    phi = np.arange(0, 331, 30)
 
     def __init__(self, vdrive_handler_file=''):
         if vdrive_handler_file == '':
@@ -18,4 +29,44 @@ class VdriveToMtex(object):
 
     def load(self):
         vdrive_handler_file = self.vdrive_handler_file
-        self.raw_input_data = pd.read_csv(vdrive_handler_file, sep='\t')
+        self.raw_data = pd.read_csv(vdrive_handler_file, sep=',')
+
+    def sort_raw_data(self):
+        """format the ducu.txt data
+
+        only the first 7 columns of data will be used
+        The data are sorted according to psi and then phi
+        """
+        raw_data_sorted = self.raw_data.sort_values(['#psi', 'phi'])
+        self.raw_data_sorted = raw_data_sorted
+
+        # get ride of first 2 columns (psi and phi
+        clean_data_sorted = np.array(raw_data_sorted.drop(['#psi', 'phi'], axis=1))
+        self.i_over_v_data_only = clean_data_sorted
+
+        a111 = self.a111
+        a200 = self.a200
+        a220 = self.a220
+        a311 = self.a311
+        a222 = self.a222
+
+        a111_flatten = a111.flatten()
+
+        # special case for psi and phi = 0
+        a111_flatten[0:12] = clean_data_sorted[0, 0]
+        # a200[:, 0] = clean_data_sorted[0, 1]
+        # a220[:, 0] = clean_data_sorted[0, 2]
+        # a311[:, 0] = clean_data_sorted[0, 3]
+        # a222[:, 0] = clean_data_sorted[0, 4]
+
+        # all over psi and phi
+        i_over_v_flatten_for_a111 = clean_data_sorted[:, 0].flatten()
+        a111_flatten[12: ] = i_over_v_flatten_for_a111[1: ]
+
+        a111 = np.reshape(a111_flatten, (12, 19))
+
+        self.a111 = a111
+        self.a200 = a200
+        self.a220 = a220
+        self.a311 = a311
+        self.a222 = a222
--- tests/project/preparation/vdrive_to_mtex_test.py
+++ tests/project/preparation/vdrive_to_mtex_test.py
@@ -16,7 +16,7 @@ class TestVDriveToMtexHandler(unittest.TestCase):
         self.data_path = os.path.abspath(os.path.join(_file_path,
                                                       '../../data/'))
         self.export_folder = self.data_path + '/temporary_folder/'
-        self.input_filename = os.path.join(self.data_path, 'ducu.txt')
+        self.input_filename = os.path.join(self.data_path, 'my_ducu.txt')
         # os.mkdir(self.export_folder)
 
         # max diff allowed to compare two arrays
@@ -40,8 +40,45 @@ class TestVDriveToMtexHandler(unittest.TestCase):
         o_handler = VdriveToMtex(vdrive_handler_file=input_file)
         o_handler.load()
 
-        raw_data = o_handler.raw_input_data
+        raw_data = o_handler.raw_data
         expected_shape = (217, 12)
         returned_shape = np.shape(raw_data)
 
         self.assertEqual(expected_shape, returned_shape)
+
+    def test_sorting_data(self):
+        """assert the data are sorted using ascending psi and phi"""
+        input_file = self.input_filename
+        o_handler = VdriveToMtex(vdrive_handler_file=input_file)
+        o_handler.load()
+        o_handler.sort_raw_data()
+
+        data_sorted = o_handler.raw_data_sorted
+        psi_column = np.array(data_sorted['#psi'])
+        phi_column = np.array(data_sorted['phi'])
+
+        expected_phi_column = np.array([0, 0, 30, 60, 90, 120, 150])
+        returned_phi_column = phi_column[0: 7]
+        self.assertTrue((expected_phi_column == returned_phi_column).all())
+
+        expected_psi_column = np.array([0, 5, 5, 5, 5, 5, 5])
+        returned_psi_column = psi_column[0: 7]
+        self.assertTrue((expected_psi_column == returned_psi_column).all())
+
+    def test_axxx_arrays(self):
+        """assert the a111, a200, a220 a311 and a222 are correctly defined"""
+        input_file = self.input_filename
+        o_handler = VdriveToMtex(vdrive_handler_file=input_file)
+        o_handler.load()
+        o_handler.sort_raw_data()
+
+        a111 = o_handler.a111
+
+        # psi and phi == 0
+        a111_expected = np.zeros((12, 19))
+        a111_expected[:, 0] = 0.81264008
+
+        for _returned, _expected in zip(a111[:, 0], a111_expected[:, 0]):
+            self.assertAlmostEqual(_returned, _expected, delta=self.maxDiff)
+
+
